7c1bd234b3bcde7709fd35c900fd00bf09c276ea
Aaron <aarona.arduino@gmail.com>
2017-08-27 15:16:40 -0500

    loops lifetimes and enums

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-enum Plan {
-    Hotel,
-    Airline,
-    PointOfInterest,
+enum Plan<'a> {
+    Hotel(&'a Hotel<'a>),
+    Airline(&'a Airline),
 }
 
-struct Hotel {
+struct Hotel<'a> {
     p: f32,
+    name: &'a str,
 }
 
 struct Airline {
     p: f32,
 }
 
-impl Cost for Hotel {
+impl<'a> Cost for Hotel<'a> {
+    fn price(&self) -> f32 {
+        self.p
+    }
+}
+
+impl Cost for Airline {
     fn price(&self) -> f32 {
         self.p
     }
@@ -23,11 +29,23 @@ trait Cost {
 }
 
 fn main() {
-    let am = Hotel{p: 200.00};
-    cost_of_hotel(am);
+    let am = Hotel{p: 200.00, name: "Some Hotel"};
+    let al = Airline{p: 800.00};
+    let mut plan: Vec<Plan> = Vec::new();
+
+    plan.push(Plan::Airline(&al));
+    plan.push(Plan::Hotel(&am));
+
+    for val in plan {
+        match val {
+            Plan::Airline(v) => cost_of_item(v),
+            Plan::Hotel(v) => cost_of_item(v),
+        }
+    }
+    println!("Name of hotel is: {}", am.name);
 }
 
-fn cost_of_hotel<T: Cost>(item: T) {
+fn cost_of_item<T: Cost>(item: &T) {
     let cost = item.price();
     println!("Cost of hotel is: {}", cost);
 }
